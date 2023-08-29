e89aaa93638dcfdffbb65ee0e7a7886f5e7f1997
paul <paul.anderson.b.garcia@gmail.com>
2023-08-29 17:32:47 +0800

    test

--- app/Helpers/SearchFilter/Procurements/PurchaseRequests.php
+++ app/Helpers/SearchFilter/Procurements/PurchaseRequests.php
@@ -281,7 +281,9 @@ class PurchaseRequests
   }
 
   private function forPurchaseOrder(){
-    $this->model->where('pr_Branch_Level1_ApprovedBy', '!=', null)->whereHas('purchaseRequestDetails', function($q){
+    $this->model->where(function($q1){
+      $q1->where('pr_Branch_Level1_ApprovedBy', '!=', null)->orWhere('pr_Branch_Level2_ApprovedBy', '!=', null);
+    })->whereHas('purchaseRequestDetails', function($q){
       $q->where('is_submitted', true)
       ->whereHas('recommendedCanvas', function($q1){
         $q1->where('canvas_Level2_ApprovedBy', '!=', null)->orWhere('canvas_Level2_CancelledBy', '!=', null);

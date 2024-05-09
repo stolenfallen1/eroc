a6bedabc636ef27c204921aeec54d507b996ca56
Jucel Estribo <jmestribo.cdg@gmail.com>
2024-05-09 14:34:12 +0800

    udpate PO TIN

--- resources/views/pdf_layout/purchaser_order.blade.php
+++ resources/views/pdf_layout/purchaser_order.blade.php
@@ -127,7 +127,7 @@
         <div class="header-text">
           <h3>{{$pdf_data['purchase_order']?$pdf_data['purchase_order']['branch']?$pdf_data['purchase_order']['branch']['name']:'':''}}</h3>
           <h5 style="margin: -20px !important;">{{$pdf_data['purchase_order']?$pdf_data['purchase_order']['branch']?$pdf_data['purchase_order']['branch']['address']:'':''}}</h5>
-          <h5>TIN 000-309-308-000</h5>
+          <h5>TIN {{$pdf_data['purchase_order']?$pdf_data['purchase_order']['branch']?$pdf_data['purchase_order']['branch']['TIN']:'':''}}</h5>
         </div>
         <img class="qr-code" src="{{ $pdf_data['qr'] }}" alt="Example Image" width="100" height="100">
       </div>

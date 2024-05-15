7dd54c839adf97a75385442b98b1bcd8281d81ff
stolenfallen1 <jquizeo_ccs@uspf.edu.ph>
2024-05-15 09:29:44 -0800

    case type fix

--- app/Http/Controllers/BuildFile/Hospital/CaseTypeController.php
+++ app/Http/Controllers/BuildFile/Hospital/CaseTypeController.php
@@ -9,6 +9,15 @@ use Illuminate\Support\Facades\DB;
 
 class CaseTypeController extends Controller
 {
+    public function list()
+    {
+        try {
+            $data = CaseType::get();
+            return response()->json($data, 200);
+        } catch (\Exception $e) {
+            return response()->json(["msg" => $e->getMessage()], 200);
+        }
+    }
 
     public function index() {
         try {

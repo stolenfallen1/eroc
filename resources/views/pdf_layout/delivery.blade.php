d169a2d1154c237cc3046eda97e1ebb00334e23e
Jucel Estribo <jmestribo.cdg@gmail.com>
2024-06-05 18:09:31 +0800

    update

--- resources/views/pdf_layout/delivery.blade.php
+++ resources/views/pdf_layout/delivery.blade.php
@@ -178,7 +178,7 @@
                 <td class="item-td" >{{ $detail['unit']['name'] }}</td>
                 <td class="item-td" >{{ number_format($detail['rr_Detail_Item_ListCost'], 2) }}</td>
                 <td class="item-td" >{{ number_format($detail['rr_Detail_Item_TotalDiscount_Amount'], 2) }}</td>
-                <td class="item-td" >{{ number_format($detail['rr_Detail_Item_Vat_Rate'], 2) }}</td>
+                <td class="item-td" >{{ number_format($detail['rr_Detail_Item_Vat_Amount'], 2) }}</td>
                 <td class="item-td" >{{ number_format($detail['rr_Detail_Item_TotalNetAmount'], 2) }}</td>
               </tr>
           @endforeach

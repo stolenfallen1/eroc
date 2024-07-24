1546c9a614f7673466c7f283e60447ca5a1de355
stolenfallen1 <jquizeo_ccs@uspf.edu.ph>
2024-07-24 15:36:11 +0800

    use / import cashier controller

--- routes/his/api_his_functions.php
+++ routes/his/api_his_functions.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\HIS\his_functions\CashierController;
 use App\Http\Controllers\HIS\his_functions\HISCashAssestmentController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HIS\his_functions\HISPostChargesController;

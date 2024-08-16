bafb6ca39fe8d08ac056fe3318ab495cfaae696e
Charlie <santandercharlie.mail@gmail.com>
2024-08-16 08:04:33 -0700

    added models

---
+++ app/Models/HIS/PatientOBGYNHistory.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Models\HIS;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class PatientOBGYNHistory extends Model
+{
+    use HasFactory;
+    protected $connection = 'sqlsrv_patient_data';
+    protected $table = 'CDG_PATIENT_DATA.dbo.PatientOBGYNHistory';
+    protected $guarded = [];
+}

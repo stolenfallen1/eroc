5f3421690a8b0f521a568a15aba66f3d7732ea8b
stolenfallen1 <jquizeo_ccs@uspf.edu.ph>
2024-09-19 15:52:32 +0800

    updates on cashier controller and minor changes on some thigns

--- routes/his/api_his_functions.php
+++ routes/his/api_his_functions.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\HIS\his_functions\CashierController;
 use App\Http\Controllers\HIS\his_functions\HISCashAssestmentController;
+use App\Http\Controllers\HIS\his_functions\SOAController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HIS\his_functions\HISPostChargesController;
 
@@ -18,13 +19,23 @@ Route::controller(HISCashAssestmentController::class)->group(function () {
     Route::post('post-cash-assessment', 'cashassessment');
     Route::put('revoke-cash-assessment', 'revokecashassessment');
 });
-
+// Cashier Routes 
 Route::controller(CashierController::class)->group(function () {
     Route::get('get-charge-item', 'populatechargeitem');
     Route::get('get-ornumber', 'getOR');
     Route::get('get-payment-codes', 'getpaymentcode');
     Route::get('get-or-sequence', 'getORSequence');
+    Route::get('get-patient-by-caseno', 'populatePatientDataByCaseNo');
+    Route::get('get-cashier-discount', 'getCashierDiscount');
+    Route::get('get-company-details', 'getCompanyDetails');
+    Route::post('get-opd-bill', 'getOPDBill');
     Route::post('cashier-settings', 'cashiersettings');
-    Route::post('save-payment', 'save');
+    Route::post('save-payment', 'saveCashAssessment');
+    Route::post('save-opbill', 'saveOPDBill');
+    Route::post('save-companybill', 'saveCompanyTransaction');
     Route::put('cancel-ornumber', 'cancelOR');
+});
+// SOA Routes
+Route::controller(SOAController::class)->group(function () {
+    Route::get('get-his-patient-soa', 'getPatientSOA');
 });

7c97d5d1c8b0d303cb72d2ad783d335a3ddfae04
Jucel Estribo <jmestribo.cdg@gmail.com>
2024-10-16 17:00:14 +0800

    update report

--- app/Models/MMIS/reports/InventoryReportPurchaseSubsidiaryLedger.php
+++ app/Models/MMIS/reports/InventoryReportPurchaseSubsidiaryLedger.php
@@ -1,7 +1,7 @@
 <?php
 
-namespace App\Models\MMIS\reports;
 
+namespace App\Models\MMIS\reports;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use DB;

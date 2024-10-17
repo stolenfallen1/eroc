6d297f16fac726c56e360fdb7856a605a091ee33
stolenfallen1 <jquizeo_ccs@uspf.edu.ph>
2024-10-17 15:55:13 +0800

    medicine and supplies--opd

---
+++ app/Http/Controllers/HIS/his_functions/opd_specific/OPDMedicinesSuppliesController.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Http\Controllers\HIS\his_functions\opd_specific;
+
+use App\Http\Controllers\Controller;
+use App\Models\BuildFile\FMS\TransactionCodes;
+use App\Models\BuildFile\Itemmasters;
+use App\Models\BuildFile\Warehouseitems;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+
+class OPDMedicinesSuppliesController extends Controller
+{
+    //
+    public function medicineSuppliesList(Request $request) 
+    {
+        try {
+            $revenueCode = TransactionCodes::where("code",$request->revenuecode)->first();
+            if (!$revenueCode) {
+                return response()->json(["msg" => "Revenue code not found"], 404);
+            }
+            $warehouseItems = Warehouseitems::where('warehouse_Id', $request->warehouseID)->pluck('item_Id');
+            $warehouseItemsArray = $warehouseItems->toArray();
+            if (empty($warehouseItemsArray)) {
+                return response()->json(["msg" => "Warehouse items not found"], 404);
+            }
+
+            $priceColumn = $request->patienttype == 1 ? 'item_Selling_Price_Out' : 'item_Selling_Price_In';
+            $items = Itemmasters::with(['wareHouseItems' => function ($query) use ($request, $priceColumn) {
+                $query->where('warehouse_Id', $request->warehouseID)
+                    ->select('id', 'item_Id', DB::raw("$priceColumn as price"));
+            }])
+            ->whereIn('id', $warehouseItemsArray) 
+            ->orderBy('item_name', 'asc');
+
+            if($request->keyword) {
+                $items->where('item_name','LIKE','%'.$request->keyword.'%');
+            }
+            $page  = $request->per_page ?? '15';
+            return response()->json($items->paginate($page), 200);
+
+        } catch(\Exception $e) {
+            return response()->json(["msg" => $e->getMessage()], 500);
+        }
+    }
+}

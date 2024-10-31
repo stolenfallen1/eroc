65500aa49441651ba9a13bcfef4afa8da2f69e41
Jucel Estribo <jmestribo.cdg@gmail.com>
2024-10-31 11:35:55 +0800

    update delivery printout

--- app/Helpers/SearchFilter/Procurements/PurchaseOrders.php
+++ app/Helpers/SearchFilter/Procurements/PurchaseOrders.php
@@ -54,14 +54,14 @@ class PurchaseOrders
               else if($column == 'pr_number'){
                 $q->orWhereHas('purchaseRequest', function($q2) use($keyword){
                   $q2->whereRaw("CONCAT(pr_Document_Prefix,'',pr_Document_Number,'',pr_Document_Suffix) = ?", $keyword)
-                  ->orWhere('pr_Document_Number', 'LIKE' , '%' . $keyword . '%');
+                  ->orWhere('pr_Document_Number', 'LIKE' , '' . $keyword . '%');
                 });
               }
                   // $q->where('pr_Document_Number', 'LIKE' , '%' . $keyword . '%');
           }
       });
     }else{
-      // $this->model->where('po_Document_number', 'like', "000%");
+      $this->model->where('po_Document_number', 'like', "000%");
     }
   }
   

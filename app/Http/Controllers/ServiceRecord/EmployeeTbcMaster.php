9a76d69be879bc0457915610af3252502891d778
Charlie Santander <santandercharlie.mail@gmail.com>
2025-01-10 16:36:39 +0800

    updated employee service record bugs fixes

--- app/Http/Controllers/ServiceRecord/EmployeeTbcMaster.php
+++ app/Http/Controllers/ServiceRecord/EmployeeTbcMaster.php
@@ -3,7 +3,6 @@
 namespace App\Http\Controllers\ServiceRecord;
 
 use DB;
-use illuminate\Support\Facades\Log;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 
@@ -12,72 +11,45 @@ class EmployeeTbcMaster extends Controller
     //
     public function getEmployeeDetail(Request $request) {
         try {
-            $lastname   = strtoupper($request->input('lastname')); 
-            $firstname  = strtoupper($request->input('firstname'));
-            $per_page = $request->input('per_page', 10);
-    
-            if (!empty($lastname) && !empty($firstname)) {
+            echo $request->input('lastName');
+            $lastname   = strtoupper($request->input('lastName'));
+            $firstname  = strtoupper($request->input('firstName'));
+            $per_page = Request()->per_page ?? 10;
+            if(!empty($lastname) && !empty($firstname)) {
                 $employeeDetail = DB::connection('sqlsrv_service_record')->table('tbcMaster')
                                     ->select('EmpNum', 'Lastname', 'Firstname')
                                     ->where('Lastname', 'like', '%' . $lastname . '%')
                                     ->where('Firstname', 'like', '%' . $firstname . '%')
                                     ->paginate($per_page);
-    
-                if ($employeeDetail->total() === 0) {
-                    return response()->json(['message' => 'No records found'], 404);
+                if (empty($employeeDetail)) {
+                    return response()->json([], 200);
                 }
-                return response()->json($employeeDetail, 200);
-            } else {
-                return response()->json([
-                    'message' => 'Invalid inputs',
-                    'error' => 'Invalid inputs'
-                ], 400);
+                return response()->json($employeeDetail);
             }
         } catch (\Exception $e) {
             return response()->json(['error' => $e->getMessage()], 500);
         }
     }
-    
 
-
-    // public function getEmployeeServiceRecords() {
-    //     try{
-    //         $userRequest = $this->getUserRequest();
-    //         $serviceRecords = DB::connection('sqlsrv_service_record')->select('EXEC sp_EmployeeServiceRecord ?, ?, ?',[$userRequest['year'], $userRequest['month'], $userRequest['empnum']]);
-    //         if (empty($serviceRecords)) {
-    //             return response()->json([], 200);
-    //         }
-    //         return response()->json($serviceRecords);
-    //     } catch (\Exception $e) {
-    //         return response()->json(['error' => $e->getMessage()], 500);
-    //     }
-    // }
 
     public function getEmployeeServiceRecords() {
-        try {
+        try{
             $userRequest = $this->getUserRequest();
-    
-            $serviceRecords = DB::connection('sqlsrv_service_record')->select(
-                'EXEC sp_EmployeeServiceRecord ?, ?, ?',
-                [$userRequest['year'], $userRequest['month'], $userRequest['empnum']]
-            );
-    
+            $serviceRecords     =   DB::connection('sqlsrv_service_record')->select('EXEC sp_EmployeeServiceRecord ?, ?, ?',[$userRequest['year'], $userRequest['month'], $userRequest['empnum']]);
             if (empty($serviceRecords)) {
                 return response()->json([], 200);
             }
             return response()->json($serviceRecords);
         } catch (\Exception $e) {
-            Log::error('Error executing procedure:', ['error' => $e->getMessage()]);
             return response()->json(['error' => $e->getMessage()], 500);
         }
     }
-    
 
 
     public function getEmployeeLeaves() {
         try {
             $userRequest = $this->getUserRequest();
-            $employeeLeaves     =   DB::connection('sqlsrv_service_record')->select('SET NOCOUNT ON, EXEC sp_employee_leaves @Year = ?, @MonthName = ?, @empnum = ?',[$userRequest['year'], $userRequest['month'], $userRequest['empnum']]);
+            $employeeLeaves     =   DB::connection('sqlsrv_service_record')->select('EXEC sp_employee_leaves @Year = ?, @MonthName = ?, @empnum = ?',[$userRequest['year'], $userRequest['month'], $userRequest['empnum']]);
             if (empty($employeeLeaves)) {
                 return response()->json([], 200);
             }
@@ -91,7 +63,7 @@ class EmployeeTbcMaster extends Controller
     public function getEmployeeUnderTime() {
         try {
             $userRequest = $this->getUserRequest();
-            $employeeUdertimeSummary        =   DB::connection('sqlsrv_service_record')->select('SET NOCOUNT ON, EXEC sp_EmployeeUndertimeSummary ?, ?, ?',[$userRequest['year'], $userRequest['month'], $userRequest['empnum']]);
+            $employeeUdertimeSummary        =   DB::connection('sqlsrv_service_record')->select('EXEC sp_EmployeeUndertimeSummary ?, ?, ?',[$userRequest['year'], $userRequest['month'], $userRequest['empnum']]);
             if (empty($employeeUdertimeSummary)) {
                 return response()->json([], 200);
             }
@@ -105,7 +77,7 @@ class EmployeeTbcMaster extends Controller
     public function getEmployeeTardiness() {
         try {
             $userRequest = $this->getUserRequest();
-            $employeeTardySummary           =   DB::connection('sqlsrv_service_record')->select('SET NOCOUNT ON, EXEC sp_EmployeeTardySummary ?, ?, ?',[$userRequest['year'], $userRequest['month'], $userRequest['empnum']]);
+            $employeeTardySummary           =   DB::connection('sqlsrv_service_record')->select('EXEC sp_EmployeeTardySummary ?, ?, ?',[$userRequest['year'], $userRequest['month'], $userRequest['empnum']]);
             if (empty($employeeTardySummary)) {
                 return response()->json([], 200);
             }
@@ -118,7 +90,7 @@ class EmployeeTbcMaster extends Controller
 
     public function getPainLeaves() {
         try{
-            $paidLeaves = DB::connection('sqlsrv_service_record')->select('SET NOCOUNT ON, EXEC sp_EmployeePaidLeaves');
+            $paidLeaves = DB::connection('sqlsrv_service_record')->select('EXEC sp_EmployeePaidLeaves');
             if(empty($paidLeaves)) {
                 return response()->json([], 200);
             }
@@ -132,7 +104,7 @@ class EmployeeTbcMaster extends Controller
 
     public function getNonPaidLeave() {
         try{
-            $nonPaidLeaves = DB::connection('sqlsrv_service_record')->select('SET NOCOUNT ON, EXEC sp_EmployeeWithoutPaidLeaves');
+            $nonPaidLeaves = DB::connection('sqlsrv_service_record')->select('EXEC sp_EmployeeWithoutPaidLeaves');
             if(empty($nonPaidLeaves)) {
                 return response()->json([], 200);
             }
@@ -146,7 +118,7 @@ class EmployeeTbcMaster extends Controller
     public function getEmployeeOT() {
         try{
             $userRequest = $this->getUserRequest();
-            $employeeOT = DB::connection('sqlsrv_service_record')->select('SET NOCOUNT ON, EXEC sp_EmployeeOvertimeSummary ?, ?, ?', [$userRequest['year'], $userRequest['month'], $userRequest['empnum']]);
+            $employeeOT = DB::connection('sqlsrv_service_record')->select('EXEC sp_EmployeeOvertimeSummary ?, ?, ?', [$userRequest['year'], $userRequest['month'], $userRequest['empnum']]);
             if(empty($employeeOT)) {
                 return response()->json([], 200);
             }
@@ -162,7 +134,7 @@ class EmployeeTbcMaster extends Controller
         $requestParam = [
             'year'  => $request->input('year'),
             'month' => $request->input('month'),
-            'empnum' => $request->input('empnum')
+            'empnum' => $request->input('empId')
         ];
         return $requestParam;
     }

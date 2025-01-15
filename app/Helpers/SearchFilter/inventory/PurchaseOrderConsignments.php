2cdd008a31b3234109c25bcc1c9f37d357c7d417
Jucel Estribo <69851912+jmestribo@users.noreply.github.com>
2025-01-15 17:01:51 +0800

    Update PurchaseOrderConsignments.php

--- app/Helpers/SearchFilter/inventory/PurchaseOrderConsignments.php
+++ app/Helpers/SearchFilter/inventory/PurchaseOrderConsignments.php
@@ -134,11 +134,7 @@ class PurchaseOrderConsignments
             });
           } elseif ($column === 'invoice') {
             $query->orWhere('invoice_no', 'LIKE', '%' . $keyword . '%');
-          } elseif ($column === 'dr_number') {
-            $query->orWhereHas('rr_consignment_master', function ($q2) use ($keyword) {
-              $q2->orWhere('rr_Document_Delivery_Receipt_No', 'LIKE', '%' . $keyword . '%');
-            });
-          }
+          } 
         }
       });
     }
